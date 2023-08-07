c06ebcad4bfcea509b645a4ae9e0267817b0fded
rizkyperdananst <riskiperdana345@gmail.com>
2023-08-07 10:30:44 +0700

    Learn-Use-Effect

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,4 +1,4 @@
-import { Fragment, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import Button from "../components/Elements/Button";
 import CardProduct from "../components/Fragments/CardProduct";
 import Counter from "../components/Fragments/Counter";
@@ -30,12 +30,22 @@ const products = [
 const email = localStorage.getItem('email');
 
 const ProductsPage = () => {
-     const [cart, setCart] = useState([
-          {
-               id: 1,
-               qty: 1,
-          },
-     ]);
+     const [cart, setCart] = useState([]);
+     const [totalPrice, setTotcalPrice] = useState(0);
+     useEffect(() => {
+          setCart(JSON.parse(localStorage.getItem("cart")) || []);
+     }, []);
+
+     useEffect(() => {
+          if(cart.length > 0) {
+               const sum = cart.reduce((acc, item) => {
+                    const product = products.find((product) => product.id === item.id);
+                    return acc + product.price * item.qty;
+               }, 0);
+               setTotcalPrice(sum);
+               localStorage.setItem("cart", JSON.stringify(cart));
+          }
+     }, [cart]);
 
      const handleLogout = () => {
           localStorage.removeItem('email');
@@ -111,13 +121,17 @@ const ProductsPage = () => {
                                              </tr>
                                         );
                                    })}
+                                   <tr>
+                                        <td colSpan={3}><b>Total Price</b></td>
+                                        <td><b>Rp{" "} {totalPrice.toLocaleString("id-ID", { styles: "currency", currency: "IDR" })}</b></td>
+                                   </tr>
                               </tbody>
                          </table>
                     </div>
                </div>
-               <div className="flex justify-center w-100 mb-5">
+               {/* <div className="flex justify-center w-100 mb-5">
                     <Counter></Counter>
-               </div>
+               </div> */}
           </Fragment>
      )
 }
